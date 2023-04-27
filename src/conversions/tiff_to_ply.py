c795e25255f9e2d797d12d0af9c333db9704282d
Timothy Cronin <tcroniniv@gmail.com>
2023-04-27 16:57:01 -0400

    structure for reading from single files

--- src/conversions/tiff_to_ply.py
+++ src/conversions/tiff_to_ply.py
@@ -1,7 +1,6 @@
 import os
 import cv2
 import numpy as np
-from definitions import ROOT_DIR
 
 
 # creates a point cloud file (.ply) from numpy array
@@ -24,7 +23,7 @@ def createPointCloud(filename, arr):
     for point in arr:
         # progress check
         point_count += 1
-        if point_count % 1000 == 0:
+        if point_count % 10000 == 0:
             print("Point: " + str(point_count) + " of " + str(len(arr)))
 
         # create file string
@@ -49,20 +48,17 @@ def addPoints(mask, points_list, depth):
         points_list.append(point)
 
 
-def tiff_to_ply(folder, output_name):
+def tiff_to_ply(path, output_name, dir=True):
     # tweakables
     slice_thickness = .2  # distance between slices
     xy_scale = 1  # rescale of xy distance
 
     # load images
-    files = os.listdir(folder)
-    images = []
-    for file in files:
-        if file[-4:] == ".tif" or file[-5:] == ".tiff":
-            img = cv2.imread(os.path.join(folder, file), cv2.IMREAD_GRAYSCALE)
-            img = cv2.resize(img, (100, 100))  # change here for more or less resolution
-            images.append(img)
-
+    if dir:
+        images = get_images_from_dir(path, [".tif", ".tiff"])
+    else:
+        print("reading tif with multiple images not available")
+        exit(1)
     # keep a blank mask
     blank_mask = np.zeros_like(images[0], np.uint8)
 
@@ -73,19 +69,16 @@ def tiff_to_ply(folder, output_name):
         # mask
         mask = cv2.inRange(image, 0, 100)
 
-        # show
-        cv2.imshow("Mask", mask)
         cv2.waitKey(1)
         masks.append(mask)
     masks.append(blank_mask)
-    cv2.destroyAllWindows()
 
     # go through and get points
     depth = 0
     points = []
     for index in range(1, len(masks)-1):
         # progress check
-        print("Index: " + str(index) + " of " + str(len(masks)))
+        # print("Index: " + str(index) + " of " + str(len(masks)))
 
         # get three masks
         prev = masks[index - 1]
@@ -128,3 +121,18 @@ def tiff_to_ply(folder, output_name):
 
     # save to point cloud file
     return createPointCloud(output_name, points)
+
+
+def get_images_from_dir(folder, file_endings):
+    files = os.listdir(folder)
+    images = []
+    is_file = True
+    for file in files:
+        is_file = any(file.endswith(end) for end in file_endings)
+
+        if is_file:
+            img = cv2.imread(os.path.join(folder, file), cv2.IMREAD_GRAYSCALE)
+            # change here for more or less resolution
+            img = cv2.resize(img, (100, 100))
+            images.append(img)
+    return images
